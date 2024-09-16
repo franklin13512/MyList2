305458351d7c5131debeda064b853190a7e62cbd
Florence Murkfulsky <162458889+franklin13512@users.noreply.github.com>
2024-09-16 18:32:56 +0800

    Add files via upload

---
+++ Lab.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProject
+{
+    internal class Lab
+    {
+        static void Main()
+        {
+            MyList1<string> NameList = new MyList1<string>();
+            NameList.AddAtLast("A");
+            NameList.AddAtLast("B");
+            NameList.AddAtLast("C");
+            NameList.AddAtLast("D");
+            NameList.AddAtLast("E");
+            NameList.AddAtLast("F");
+            NameList.AddAtLast("r");
+            //NameList.AddAtLast("y");
+            //NameList.AddAtLast("k");
+
+            Console.WriteLine(NameList.Number);
+            Console.WriteLine(NameList.Capacity);
+
+            for(int i = 0;i < NameList.Number ;i++)
+            {
+                Console.WriteLine(NameList[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyList1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProject
+{
+    internal class MyList1<T>
+    {
+        private T[] Data = new T[4];
+        private int Count = 0;
+
+        //访问动态数组容量
+        public int Capacity
+        {
+            get { return Data.Length; }
+        }
+
+        //访问动态数组元素量
+        public int Number
+        {
+            get { return Count; }
+        }
+
+        public void Add(int Index, T Value) 
+        {
+            if (Index < 0 || Index > Count ) 
+            {
+                throw new ArgumentOutOfRangeException("溢出");
+
+            }
+            else if (Count >= Data.Length)
+            {
+                throw new ArgumentOutOfRangeException("溢出");
+            }
+            else
+            {
+                for (int i = Count - 1; Index <= i; i--)
+                {
+                    Data[i + 1] = Data[i];
+                }
+                Data[Index] = Value;
+                Count++;
+
+                if (Count == Data.Length)
+                {
+                    T[] NewList = new T[Data.Length * 2];
+
+                    for (int i = 0; i < Data.Length; i++)
+                    {
+                        NewList[i] = Data[i];
+                    }
+                    Data = NewList;
+                }
+            }
+        }
+
+        public void AddAtLast(T Value)
+        {
+            Add(Count, Value);
+        }
+
+        public void AddAtFirst(T Value)
+        {
+            Add(0, Value);
+        }
+
+
+        public T this[int index]
+        {
+            get { return Data[index]; }
+            set
+            {
+                Data[index] = value;
+            }
+        }
+    }
+}
